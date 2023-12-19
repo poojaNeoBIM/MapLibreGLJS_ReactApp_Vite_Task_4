558701fc451859941f8cd997bd2a25d32b965492
“Pooja <pooja@neobim.ai>
2023-12-19 12:19:15 +0100

    final commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      '@babylonjs/core',
+      '@babylonjs/loaders',
+      '@babylonjs/materials',
+      'maplibre-gl'
+    ],
+    // Optionally, specify any dependencies to exclude
+    // exclude: ['some-package']
+  }
 });
